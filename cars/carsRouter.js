5bbb4cf726796459a49eee258b5b8a2438372056
ruwaidah <Ruwidah.riyadh@gmail.com>
2019-12-10 20:06:18 -0600

    build endpoint to get car by Id

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -8,6 +8,29 @@ router.get("/", (req, res) => {
     .select("*")
     .then(cars => {
       res.status(200).json(cars);
+    })
+    .catch(error => {
+      res.status(500).json({
+        errorMessage: "Error getting all cars"
+      });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  db("cars")
+    .select("*")
+    .where({ id: req.params.id })
+    .then(car => {
+      if (car.length > 0) res.status(200).json(car[0]);
+      else
+        res.status(400).json({
+          message: "no Car With this ID"
+        });
+    })
+    .catch(error => {
+      res.status(500).json({
+        errorMessage: "Error getting car data"
+      });
     });
 });
 module.exports = router;
